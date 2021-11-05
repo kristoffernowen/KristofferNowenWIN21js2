3f9b419cd9042d66c3bc32f351222344c4ca50fc
Kristoffer Nowen <kristoffer.nowen@utb.ecutbildning.se>
2021-11-05 13:07:19 +0100

    fredag-lunch

--- uppgift-2/customer-page/src/views/RegisterCustomers.js
+++ uppgift-2/customer-page/src/views/RegisterCustomers.js
@@ -7,16 +7,15 @@ const RegisterCustomers = () => {
 
     const [hisFirstName, setHisFirstName] = useState()
     const nameHandler = (e) => { setHisFirstName(e.target.value)}
-    const [hisLastName, setHisLastName] = useState()
-    const lastNameHandler = (e) => { setHisLastName(e.target.value)}
+    
 
     
 
     const [validName, setValidName] = useState()
-    // const nameRegex = /^[a-öA-Ö]{2,}\-?([a-öA-Ö]{2,})?$/
-    const validateThis = (e) => {if (e.target.value.length  < 2) {
-                //funkar inte denna regex eller formen
-                 document.getElementById("firstNameResponse").innerText = 'Namnet måste innehålla minst två tecken'
+    const nameRegex = /^[A-Z,ÅÄÖ][a-öA-Ö]+(-[A-Z,ÅÄÖ][a-ö]+)?$/
+    const validateThis = (e) => {if (!nameRegex.test(e.target.value)) {
+                
+                 document.getElementById("firstNameResponse").innerText = 'Namnet måste innehålla minst två bokstäver, börja med stor bokstav. Skriv dubbelnamn Aa-Aa'
                  document.getElementById("firstNameResponse").style.color ="red"
                  setValidName(false)   
              } else {
@@ -43,30 +42,41 @@ const RegisterCustomers = () => {
     //     }        
     // }
 
-    const customerObject = { firstName: hisFirstName, lastName: hisLastName }
-    // console.log(theName)
+   
 
-
-
+    const [hisLastName, setHisLastName] = useState()
+    const lastNameHandler = (e) => { setHisLastName(e.target.value)}
+    const [validLastName, setValidLastName] = useState()
 
     // Passing Arguments to Event Handlers, finns guide på react sidan
 
-    // function validateThat(e) {
+    function validateThat(e) {
 
 
-    //     if (e.target.value.length < 2) {
+        if (!nameRegex.test(e.target.value)) {
 
-    //         document.getElementById("lastNameResponse").innerText = 'Namnet måste innehålla minst två tecken'
-    //         document.getElementById("lastNameResponse").style.color ="red"
+            document.getElementById("lastNameResponse").innerText = 'Namnet måste innehålla minst två bokstäver, börja med stor bokstav. Skriv dubbelnamn Aa-Aa'
+            document.getElementById("lastNameResponse").style.color ="red"
+            setValidLastName(false)
+        } else {
 
-    //     } else {
+            document.getElementById("lastNameResponse").innerText = 'Giltigt namn'
+            document.getElementById("lastNameResponse").style.color="green"
+            setValidLastName(true)
+            lastNameHandler(e)
+        }
 
-    //         document.getElementById("lastNameResponse").innerText = 'Giltigt namn'
-    //         document.getElementById("lastNameResponse").style.color="green"
-    //     }
-
-    // }
+    }
     // const emailResponse = document.getElementById('emailResponse')
+    const [hisEmail, setHisEmail] = useState()
+    const emailHandler = (e) => { setHisEmail(e.target.value)}
+    
+
+    
+
+    const [validEmail, setValidEmail] = useState()
+
+
     function validateEmail(e) {
         const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         if (!regEx.test(e.target.value)) {
@@ -74,29 +84,47 @@ const RegisterCustomers = () => {
             // emailResponse.style.color="red"
             document.getElementById('emailResponse').innerText = 'ogiltig e-postadress'
             document.getElementById('emailResponse').style.color = "red"
+            setValidEmail(false)
         } else {
             document.getElementById('emailResponse').innerText = "giltig e-postadress"
             document.getElementById('emailResponse').style.color = "green"
+            setValidEmail(true)
+            emailHandler(e)
         }
     }
+    const customerObject = { firstName: hisFirstName, lastName: hisLastName, email: hisEmail }
+    // console.log(theName)
+
+    let jsonCustomer = JSON.stringify(customerObject)
+            
+    //         async function postCustomer() {
+    //                 await fetch('https://ecexam-webapi.azurewebsites.net/api/customers',
+    //                     {
+    //                         headers: {
+    //                             'content-Type': 'application/json'
+    //                         },
+    //                         method: 'post',
+    //                         body: jsonCustomer
+    //                     })
+    //             }    //   detta går in på knappens att göra på click/submit
+
+                // https://ecexam-webapi.azurewebsites.net/swagger/index.html     här finns apiet, men måste ha annat slut för fetch/fetchpost
 
     const buttonHandler = (e) => { 
-        if (validName) console.log(customerObject, validName)
-        else
-        console.log('ogiltiga uppgifter', validName)
-        e.preventDefault()
-         }
-    // let jsonCustomer = JSON.stringify(customerObject)
-    // async function postCustomer() {
-    //     await fetch('',
-    //         {
-    //             headers: {
-    //                 'content-Type': 'application/json'
-    //             },
-    //             method: 'post',
-    //             body: jsonCustomer
-    //         })
-    // }    //   detta går in på knappens att göra på click
+        if (validName && validLastName && validEmail) {
+            console.log(customerObject, 'firstName: ' + validName, 'lastName: ' + validLastName, 'email: ' + validEmail) 
+            document.getElementById('submitResponse').innerText = "Du har skickat dina uppgifter"
+            console.log(jsonCustomer)
+            // postCustomer()
+            
+        }
+        else {
+            console.log('ogiltiga uppgifter', 'firstName: ' + validName, 'lastName: ' + validLastName, 'email: ' + validEmail)
+            e.preventDefault()
+            document.getElementById('submitResponse').innerText = "Du har lämnat ogiltiga uppgifter och kan inte registrera dig"
+        }
+    }
+    
 
 
 
@@ -105,27 +133,25 @@ const RegisterCustomers = () => {
         <div>
             <Navbar />
             <div className="d-flex justify-content-center">
-                <form className="mb-3 ">
+                <form className="mb-3 "  autoComplete="off" >
                     <div className="mb-3">
                         <label htmlFor="firstName" className="form-label">Förnamn</label>
-                        <input type="text" id="firstName" onChange={validateThis}  className="form-control" aria-describedby="emailHelp" />
+                        <input type="text" id="firstName" name="firstName" onChange={validateThis}  className="form-control" aria-describedby="emailHelp" />
                         <div id="firstNameResponse" className="form-text">Namnet måste innehålla minst två tecken.</div>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="lastName" className="form-label">Efternamn</label>
-                        <input type="text" className="form-control" id="lastName" onChange={lastNameHandler} />
+                        <input type="text" className="form-control" id="lastName" name="lastName" onChange={validateThat} />
                         <div id="lastNameResponse" className="form-text">Namnet måste innehålla minst två tecken.</div>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="emailAdress" className="form-label">E-postadress</label>
-                        <input type="text" className="form-control" id="emailAdress" onChange={validateEmail} />
+                        <input type="text" className="form-control" id="emailAdress" name="emailAdress" onChange={validateEmail} />
                         <div id="emailResponse" className="form-text">Skriv en giltig e-postadress</div>
                     </div>
-                    <div className="mb-3 form-check">
-                        <input type="checkbox" className="form-check-input" id="iAprove" />
-                        <label className="form-check-label" htmlFor="iAprove">Jag vill registrera mina uppgifter hos er</label>
-                    </div>
-                    <button id="submitButton" type="button" onClick={buttonHandler} className="btn btn-primary">Skicka</button>
+                    
+                    <button id="submitButton" type="button" onClick={buttonHandler} className="btn btn-primary">Registrera</button>
+                    <div id="submitResponse" className="form-text"></div>
                 </form>
 
                 {/* <Form /> */}
